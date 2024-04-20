41404fd2ddb39ee32eecbdd3182e015789b21383
Josh Pion <jp8117@gmail.com>
2024-04-20 14:12:41 -0400

    PartC

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ app.secret_key = 'SIH*v-6u)c>q<;;h&);cRw,1E_CO8>'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pass
+        guess = int(request.form['guess'])
+        if guess < session['low_value'] or guess > session['high_value']:
+            message = f'Invalid Selection! Please choose a number between {session['low_value']} and {session['high_value']}!'
+        elif guess == session['magic_number']:
+            message = 'A victory message'
+        elif guess > session['magic_number']:
+            session['high_value'] = guess
+            message = 'too high. Try again.'
+        elif guess < session['magic_number']:
+            session['low_value'] = guess
+            message = 'too low. Try again.'
     else:
         low_value = 1
         high_value = 50
