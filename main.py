53b0f376b1ce1c6a53b31185f8cbe6f6baecdd05
Josh Pion <jp8117@gmail.com>
2024-04-20 14:27:39 -0400

    PartD

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def index():
         if guess < session['low_value'] or guess > session['high_value']:
             message = f'Invalid Selection! Please choose a number between {session['low_value']} and {session['high_value']}!'
         elif guess == session['magic_number']:
+            session['still_guessing'] = False
             message = 'A victory message'
         elif guess > session['magic_number']:
             session['high_value'] = guess
@@ -22,6 +23,7 @@ def index():
     else:
         low_value = 1
         high_value = 50
+        session['still_guessing'] = True
         session['magic_number'] = random.randint(low_value, high_value)
         session['low_value'] = low_value
         session['high_value'] = high_value
